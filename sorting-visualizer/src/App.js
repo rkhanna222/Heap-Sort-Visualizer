0327afe855030d67804da0beb567c7e7014bb749
Raghav Khanna <khanna.ra@northeastern.edu>
2022-08-17 18:10:31 -0400

    Creating the forlder structure

--- sorting-visualizer/src/App.js
+++ sorting-visualizer/src/App.js
@@ -1,25 +1,141 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import './AppDark.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import Controls from './components/molecules/Controls';
+import TopBar from './components/organisms/TopBar';
+import AppDrawer from './components/organisms/AppDrawer';
+import SortVisualizer from './components/organisms/SortVisualizer';
+
+import HeapSort, {
+  HeapSortKey,
+  HeapSortDesc
+} from './algorithms/HeapSort';
+
+
+class App extends Component {
+  state = {
+    darkMode: false,
+    array: [],
+    arraySize: 5,
+    trace: [],
+    algorithm: "Heap Sort",
+    appDrawerOpen: false
+  };
+
+  ALGORITHM = {
+    'Heap Sort': HeapSort
+  };
+
+  ALGORITHM_KEY = {
+    'Heap Sort': HeapSortKey
+  };
+
+  ALGORITHM_DESC = {
+    'Heap Sort': HeapSortDesc,
+  };
+
+  componentDidMount() {
+    this.generateRandomArray();
+  }
+
+  generateRandomArray = () => {
+    // Generate pseudo-random number between 1 and max
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max)) + 1;
+    }
+
+    // Generate an array of length max
+    const array = Array(this.state.arraySize)
+      .fill(0)
+      .map(() => getRandomInt(this.state.arraySize * 5));
+
+    this.setState(
+      {
+        array,
+        trace: []
+      },
+      this.createTrace
+    );
+  };
+
+  handleAlgorithmChange = (algorithm) => {
+    this.setState({ algorithm }, this.generateRandomArray);
+  };
+
+  handleArraySizeChange = (size) => {
+    size = Number(size);
+    size = size > 100 ? 100 : size;
+    size = size < 0 ? 0 : size;
+    this.setState({ arraySize: size }, this.generateRandomArray);
+  };
+
+  createTrace = () => {
+    const numbers = [...this.state.array];
+    const sort = this.ALGORITHM[this.state.algorithm];
+    if (sort) {
+      const trace = sort(numbers);
+      this.setState({ trace });
+    }
+  };
+
+  toggleDarkMode = () => {
+    this.setState((prevState) => ({ darkMode: !prevState.darkMode }));
+  };
+
+  toggleAppDrawer = () => {
+    this.setState((prevState) => ({
+      appDrawerOpen: !prevState.appDrawerOpen
+    }));
+  };
+
+  render() {
+    let theme = `App`;
+    if (this.state.darkMode) theme += ` App_dark`;
+    if (this.state.appDrawerOpen) theme += ` App_modal_open`;
+
+    const colorKey = this.ALGORITHM_KEY[this.state.algorithm];
+    const desc = this.ALGORITHM_DESC[this.state.algorithm];
+
+    const controls = (
+      <Controls
+        onGenerateRandomArray={this.generateRandomArray}
+        algorithm={this.state.algorithm}
+        onAlgorithmChange={this.handleAlgorithmChange}
+        arraySize={this.state.arraySize}
+        onArraySizeChange={this.handleArraySizeChange}
+        onToggleDarkMode={this.toggleDarkMode}
+        darkMode={this.state.darkMode}
+      />
+    );
+
+    return (
+      <div className={theme}>
+        <TopBar
+          drawerOpen={this.state.appDrawerOpen}
+          toggleDrawer={this.toggleAppDrawer}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {controls}
+        </TopBar>
+
+        <AppDrawer
+          open={this.state.appDrawerOpen}
+          closeDrawer={this.toggleAppDrawer}
+        >
+          {controls}
+        </AppDrawer>
+
+        <main className="App__Body">
+          <SortVisualizer
+            array={this.state.array}
+            trace={this.state.trace}
+            colorKey={colorKey}
+            desc={desc}
+          />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
